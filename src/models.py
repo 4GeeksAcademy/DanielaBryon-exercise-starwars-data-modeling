033ffe0e7362f3fb3980be1fb8f45c32b5db61e4
GuyFawkess <80330724+GuyFawkess@users.noreply.github.com>
2024-10-24 15:57:26 +0000

    trabajo terminado

--- src/models.py
+++ src/models.py
@@ -1,29 +1,66 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, Enum
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+class User(Base):
+    __tablename__ = 'user'
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    username = Column(String(80), unique=True, nullable=False)
+    firstname = Column(String(60), nullable=False)
+    lastname = Column(String(100), nullable=False)
+    email = Column(String(40), unique=True, nullable=False)
+    address = Column(String(250), nullable=False)
+    phone = Column(String(40), nullable=False)
+    password = Column(String(80), nullable=False)
+    #relaciones
+    #ATENCION: quise utilizar el backref de la clase, pero no se si lo he implementado bien
+    likes = relationship('Favourites', backref='likes')
+
+class Planets(Base):
+    __tablename__ = 'planets'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(250), unique=True, nullable=False)
+    gravedad = Column(String(60))
+    diametro = Column(String(60))
+    poblacion = Column(String(80))
+    descripcion = Column(String(250))
+    #relaciones
+
+    likes = relationship('Favourites', back_populates='planets')
+
+class Characters(Base):
+    __tablename__ = 'characters'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(120), unique=True, nullable=False)
+    peso = Column(String(70))
+    color_ojos = Column(String(60))
+    sexo = Column(Enum('Men','Women','NaN'), nullable=False)
+    color_pelo = Column(String(50))
+    #relaciones
+
+    likes = relationship('Favourites', back_populates='characters')
+
+class Favourites(Base):
+    __tablename__ = 'favourites'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    id_planetas = Column(Integer, ForeignKey('planets.id'),  nullable=True)
+    id_personajes = Column(Integer, ForeignKey('characters.id'),  nullable=True)
+    id_user = Column(Integer, ForeignKey('user.id'))
+    #relaciones
+
+    planets = relationship('Planets', back_populates="likes")
+    characters = relationship('Characters', back_populates='likes')
+
+
 
     def to_dict(self):
         return {}
